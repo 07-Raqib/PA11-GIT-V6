c542b275e1ef98ffa3225a9125be6ac5607834c1
07-Raqib <91867711+07-Raqib@users.noreply.github.com>
2021-10-20 14:12:53 +0800

    Added Unity Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public int score = 0;
+    public Text txt_score;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(transform.position.y >=-4 && transform.position.y <= 4)
+            transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        else if(transform.position.y > 4)
+            transform.position = new Vector3(transform.position.x,4,transform.position.z);
+        else if (transform.position.y < -4)
+            transform.position = new Vector3(transform.position.x, -4, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+    public void AddScore(int addScore)
+    {
+        //score += addScore;
+        //txt_score.text = "Score: "+ score;
+    }
+}
